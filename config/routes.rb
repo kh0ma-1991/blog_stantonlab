848925295279993e0948db53e731fa9071e0d26a
kh0ma <khomenko.dp@gmail.com>
2017-11-17 00:48:10 +0200

    admin postl

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     root 'dashboard#index'
   end
 
-  resources :posts
+  resources :posts, only: [:show, :index]
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
